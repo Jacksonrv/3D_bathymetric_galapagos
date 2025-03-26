801b71ddb082a4b7c2d6b5b5377bcf4c085abe54
Jackson Vaughn <kw24171@bristol.ac.uk>
2025-03-26 13:41:05 +0000

    forgot chlorophyll link

--- 3D_galap_bthy_v1_1.py
+++ 3D_galap_bthy_v1_1.py
@@ -30,7 +30,7 @@ def download_and_load_csv(url, filename):
 # === URLs for preprocessed data ===
 #URLs must end with =1
 urls = {
-    "chl_log": "",
+    "chl_log": "https://www.dropbox.com/scl/fi/yrpyq75j5mky1l4yj9tzu/chl_log.npy?rlkey=zu7osjr41uz82matcf96hxq5o&st=ames4ds7&dl=1",
     "corals": "https://www.dropbox.com/scl/fi/1oycf677oxyie6lrtnq74/corals.csv?rlkey=9qibrb6348irh6s19lr38emzp&st=kausdzf9&dl=1",
     "elev": "https://www.dropbox.com/scl/fi/lg4zvnex8jfa2mx7mdv0d/elev_cropped.npy?rlkey=v7u36a1434cjknbvoamj9cjfn&st=4n46rcd3&dl=1",
     "land": "https://www.dropbox.com/scl/fi/su6nnm3as2ng58a2uorjw/land_elev.npy?rlkey=sbcild64c7w2scawcl92zgvsa&st=dzzuq7u2&dl=1",
